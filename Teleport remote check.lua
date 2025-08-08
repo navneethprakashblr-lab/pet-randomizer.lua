a6cf41485e63bf9d68d3f3f1eb86861c68d07182
navneethprakashblr-lab <navneethprakashblr@gmail.com>
2025-08-08 13:28:59 +0530

    Update Teleport remote check.lua

--- Teleport remote check.lua
+++ Teleport remote check.lua
@@ -1,4 +1,4 @@
 local teleportRemote = game.ReplicatedStorage:FindFirstChild("TeleportToPrivateServer")
 if teleportRemote then
-    teleportRemote:FireServer("PlaceID_or_ServerCode")
+    teleportRemote:FireServer("6ecc336c-ff8c-4d3b-bd70-94e35c571d11")
 end
